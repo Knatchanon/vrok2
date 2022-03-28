ac77119054384a55be8c388d5dbbd8478e792f3f
Knatchanon <knatchanon@netizen.co.th>
2022-03-28 09:01:01 +0000

    Database config file

---
+++ db_config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    APIKEY: '',
+    db_user: 'supleman@hack2build',
+    db_password: 'Stcc1234',
+    db_type: 'default',
+    db_server: "hack2build.database.windows.net",
+    db_database: 'vrock_db',
+    db_encrypt: true
+}
